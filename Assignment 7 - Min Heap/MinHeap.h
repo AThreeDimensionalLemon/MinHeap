378099863c3da799dc2a1cf5c6bc9023d31da147
AThreeDimensionalLemon <139825913+AThreeDimensionalLemon@users.noreply.github.com>
2024-11-01 16:29:21 -0700

    Started implementing heapify function Revert to this version if attempting to create swap function fails.

--- Assignment 7 - Min Heap/MinHeap.h
+++ Assignment 7 - Min Heap/MinHeap.h
@@ -1,46 +1,97 @@
 // 
 // Name: MinHeap.h
 // Author: Eisig Liang
-// Last update: 31/10/24
+// Last update: 1/11/24
 // Purpose: Do various things with a min heap
 // 
 
 #pragma once
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 template <typename T>
 class MinHeap {
 private:
     vector<T> heap;
-    void heapify(); //Heapify heap; used by insert() and sort()
+
+    //Helper function for code reuse and recursion
+    int heapify(T value); 
+
+    //Functions for getting relevant index in relation to a vertex's index
+    int parent(int index);
+    int left(int index);
+    int right(int index);
 
 public:
     MinHeap();
     void insert(T input);
-    vector<T> extractMin();
+    T extractMin();
     void print();
     void sort(T input);
 };
 
 template <typename T>
-void MinHeap<T>::heapify() {
+int MinHeap<T>::heapify(T value) {
+    int valueIndex = find(heap.begin(), heap.end(), value) - heap.begin();
 
+    if (valueIndex != 0 and
+        value > heap[parent(valueIndex)])
+    { //Swap if greater than parent
+        T swappedValue = heap[parent(valueIndex)];
+        heap[parent(valueIndex)] = value;
+        heap[valueIndex] = swappedValue;
+        heapify(value);
+    }
+
+    else if (left(valueIndex) < heap.size() and
+        value < heap[left(valueIndex)])
+    { //Swap if less than left child
+        T swappedValue = heap[left(valueIndex)];
+        heap[left(valueIndex)] = value;
+        heap[valueIndex] = swappedValue;
+        heapify(value);
+    }
+
+    else if (right(valueIndex) < heap.size() and
+        value < heap[right(valueIndex)])
+    { //Swap if less than right child
+        T swappedValue = heap[right(valueIndex)];
+        heap[right(valueIndex)] = value;
+        heap[valueIndex] = swappedValue;
+        heapify(value);
+    }
+}
+
+template <typename T>
+int MinHeap<T>::parent(int index) {
+    return (index - 1) / 2;
+}
+
+template <typename T>
+int MinHeap<T>::left(int index) {
+    return 2 * index + 1;
+}
+
+template <typename T>
+int MinHeap<T>::right(int index) {
+    return 2 * index + 2;
 }
 
 template <typename T>
 MinHeap<T>::MinHeap() {
-    
+
 }
 
 template <typename T>
 void MinHeap<T>::insert(T input) {
-
+    heap.push_back(input);
+    heapify(input);
 }
 
 template <typename T>
-vector<T> MinHeap<T>::extractMin() {
+T MinHeap<T>::extractMin() {
     
 }
 
@@ -51,5 +102,5 @@ void MinHeap<T>::print() {
 
 template <typename T>
 void MinHeap<T>::sort(T input) {
-
+    
 }
--- Assignment 7 - Min Heap/main.cpp
+++ Assignment 7 - Min Heap/main.cpp
@@ -3,6 +3,8 @@
 // Author: Eisig Liang
 // Last update: 31/10/24
 // Purpose: Test min heap implementation
+// Notices:
+//     1. ChatGPT was used to generated test cases
 //
 
 #include "MinHeap.h"
@@ -10,5 +12,14 @@
 using namespace std;
 
 int main() {
-    
+
+    //Test with integers
+    cout << "Testing MinHeap class with int types:" << endl;
+    MinHeap<int> intTest;
+
+    //Test insert function
+    cout << "Testing insert function: ";
+    int randomIntegers[10] = { 42, 17, 89, 3, 56, 74, 23, 65, 38, 91 };
+    for (auto integer : randomIntegers) intTest.insert(integer);
+    cout << "Done!" << endl;
 }
